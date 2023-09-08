ff01f0ee3a27170386f9788838db1867868b1824
Riccardo Balbo <riccardo0blb@gmail.com>
2023-09-08 17:35:26 +0200

    Allow memory growth

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ emcc stb_image_load.c \
 -s EXPORTED_RUNTIME_METHODS="['ccall','getValue']" \
 -s MODULARIZE=1 -s EXPORT_NAME="StbImageLoadModule" \
 -O3 \
---memory-init-file 0
+--memory-init-file 0 \
+-s ALLOW_MEMORY_GROWTH=1
 
 echo -e "\n" >> build/stb_image_load.js
 cat stb_image_load.js >> build/stb_image_load.js
